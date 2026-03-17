326356657abf124ecf5dd4a9c25d9a72ecf38194
zwscloud <samvpslio@gmail.com>
2026-03-17 16:29:49 +0530

    Add files via upload

---
+++ zws-user-add.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+############################################################
+# ZWS CLOUD Proxy Installer
+# Author: ZWS CLOUD
+# Email: info@zwscloud.com
+# Github: https://github.com/samvpslio/zws-proxy/
+# Web: https://zwscloud.com
+# If you need professional assistance, reach out to
+# https://zwscloud.com/order/contact.php
+############################################################
+
+if [ `whoami` != root ]; then
+	echo "ERROR: You need to run the script as user root or add sudo before command."
+	exit 1
+fi
+
+if [ ! -f /usr/bin/htpasswd ]; then
+    echo "htpasswd not found"
+    exit 1
+fi
+
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
+if [[ -d /etc/squid/ || -d /etc/squid3/ ]]; then
+	echo -e "${GREEN}Thank you for using ZWS CLOUD Proxy Service.${NC}"
+ 	echo
+    	SQUID_USER=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)
+	SQUID_PW=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+	echo -e "${CYAN}Username : ${SQUID_USER}${NC}"
+	echo -e "${CYAN}Password : ${SQUID_PW}${NC}"
+ 	echo -e "${CYAN}Port : 3128${NC}"
+	
+	htpasswd -b -c /etc/squid/passwd $SQUID_USER $SQUID_PW > /dev/null 2>&1
+	
+	sed -i 's/Squid proxy-caching web server/ZWS CLOUD Proxy Service/g'  /etc/squid/squid.conf
+	
+	systemctl restart squid > /dev/null 2>&1
+	systemctl restart squid3 > /dev/null 2>&1
+    exit 1
+fi
+
+
